3a7ae4b231c90489b7dfaef9f4dd353c470e5b9f
mvirk4 <mvirk@flemingcollege.ca>
2024-01-17 11:08:44 -0500

    modified both script files
    deleted print from script1.py
    added comments to scripr2.py

--- script1.py
+++ script1.py
@@ -1,3 +1,2 @@
 print("There shoul be one-- and preferably only one --obvious way to do it.")
-print("Although that way may not be obvious at first unless you're Dutch.")
 # this is the first comment
