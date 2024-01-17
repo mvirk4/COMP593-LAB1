9993334045407191263e12703c9498edf1bef782
mvirk4 <mvirk@flemingcollege.ca>
2024-01-17 11:10:49 -0500

    Revert "modified both script files"
    This reverts commit 3a7ae4b231c90489b7dfaef9f4dd353c470e5b9f.

--- script1.py
+++ script1.py
@@ -1,2 +1,3 @@
 print("There shoul be one-- and preferably only one --obvious way to do it.")
+print("Although that way may not be obvious at first unless you're Dutch.")
 # this is the first comment
